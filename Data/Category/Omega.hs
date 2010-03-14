692e33c50cd4ed8a058715f2b6dafdd8739c953f
Sjoerd Visscher <sjoerd@w3future.com>
2010-03-14 02:32:20 +0100

    Added product and coproduct to the omega category.

--- Data/Category/Omega.hs
+++ Data/Category/Omega.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE TypeFamilies, MultiParamTypeClasses, FlexibleInstances, FlexibleContexts, UndecidableInstances, RankNTypes #-}
+{-# LANGUAGE TypeFamilies, MultiParamTypeClasses, FlexibleInstances, FlexibleContexts, UndecidableInstances, RankNTypes, ScopedTypeVariables #-}
 module Data.Category.Omega where
 
 import Prelude hiding ((.), id)
@@ -14,12 +14,12 @@ data family Omega a b :: *
 
 -- Objects
 data Z = Z deriving Show
-newtype S n = S n deriving Show
+newtype S n = S { unS :: n } deriving Show
 
 -- Arrows, there's an arrow from a to b when a is less than or equal to b
 data instance Omega Z Z = IdZ
-newtype instance Omega Z (S n) = GTZ (Omega Z n)
-newtype instance Omega (S a) (S b) = StepS (Omega a b)
+newtype instance Omega Z (S n) = GTZ { unGTZ :: Omega Z n }
+newtype instance Omega (S a) (S b) = StepS { unStepS :: Omega a b }
 
 instance Apply Omega Z Z where
   IdZ $$ Z = Z
@@ -46,17 +46,40 @@ data instance Funct Omega d (FunctO Omega d f) (FunctO Omega d g) =
 instance (Dom f ~ Omega, Cod f ~ d, CategoryO (Cod f) (F f Z)) => CategoryO (Funct Omega d) (FunctO Omega d f) where
   id = OmegaNat id (const id)
 
+instance VoidColimit Omega where
+  type InitialObject Omega = Z
+  voidColimit = InitialUniversal VoidNat (OmegaNat (\VoidNat -> IdZ) (\cpt VoidNat -> GTZ (cpt VoidNat)))
 
-initObjInOmega :: Colimit (VoidF Omega) Z
-initObjInOmega = InitialUniversal VoidNat $ OmegaNat (\VoidNat -> IdZ) (\cpt VoidNat -> GTZ (cpt VoidNat))
+-- The product in omega is the minimum.
+instance PairLimit Omega Z Z where 
+  type Product Z Z = Z
+  pairLimit = TerminalUniversal (IdZ :***: IdZ) (OmegaNat fstComp (\cpt -> sndComp))
+instance (PairLimit Omega Z n, Product Z n ~ Z) => PairLimit Omega Z (S n) where 
+  type Product Z (S n) = Z
+  pairLimit = TerminalUniversal (IdZ :***: GTZ p) (OmegaNat fstComp (\cpt -> fstComp)) where
+    TerminalUniversal (_ :***: p) _ = pairLimit :: Limit (PairF Omega Z n) (Product Z n)
+instance (PairLimit Omega n Z, Product n Z ~ Z) => PairLimit Omega (S n) Z where 
+  type Product (S n) Z = Z
+  pairLimit = TerminalUniversal (GTZ p :***: IdZ) (OmegaNat sndComp (\cpt -> sndComp)) where
+    TerminalUniversal (p :***: _) _ = pairLimit :: Limit (PairF Omega n Z) (Product n Z)
+instance (PairLimit Omega a b) => PairLimit Omega (S a) (S b) where 
+  type Product (S a) (S b) = S (Product a b)
+  pairLimit = TerminalUniversal (StepS p1 :***: StepS p2) undefined where
+    TerminalUniversal (p1 :***: p2) _ = pairLimit :: Limit (PairF Omega a b) (Product a b)
 
-type family Min x y :: *
-type instance Min Z Z = Z
-type instance Min Z (S n) = Z
-type instance Min (S n) Z = Z
-type instance Min (S a) (S b) = Min a b
-
-prodLimitInOmegaZZ :: Limit (PairF Omega Z Z) (Min Z Z)
-prodLimitInOmegaZZ = TerminalUniversal (IdZ :***: IdZ) $ OmegaNat (\(f :***: s) -> f) (\cpt (f :***: s) -> f)
-prodLimitInOmegaZS :: Limit (PairF Omega Z (S n)) (Min Z (S n))
-prodLimitInOmegaZS = TerminalUniversal (IdZ :***: GTZ undefined) $ OmegaNat (\(f :***: s) -> f) (\cpt (f :***: s) -> f)
+-- The coproduct in omega is the maximum.
+instance PairColimit Omega Z Z where 
+  type Coproduct Z Z = Z
+  pairColimit = InitialUniversal (IdZ :***: IdZ) (OmegaNat fstComp (\cpt -> sndComp))
+instance (PairColimit Omega Z n, Coproduct Z n ~ n) => PairColimit Omega Z (S n) where 
+  type Coproduct Z (S n) = S n
+  pairColimit = InitialUniversal (GTZ p1 :***: StepS p2) (OmegaNat sndComp (\cpt -> sndComp)) where
+    InitialUniversal (p1 :***: p2) _ = pairColimit :: Colimit (PairF Omega Z n) (Coproduct Z n)
+instance (PairColimit Omega n Z, Coproduct n Z ~ n) => PairColimit Omega (S n) Z where 
+  type Coproduct (S n) Z = S n
+  pairColimit = InitialUniversal (StepS p1 :***: GTZ p2) (OmegaNat fstComp (\cpt -> fstComp)) where
+    InitialUniversal (p1 :***: p2) _ = pairColimit :: Colimit (PairF Omega n Z) (Coproduct n Z)
+instance (PairColimit Omega a b) => PairColimit Omega (S a) (S b) where 
+  type Coproduct (S a) (S b) = S (Coproduct a b)
+  pairColimit = InitialUniversal (StepS p1 :***: StepS p2) undefined where
+    InitialUniversal (p1 :***: p2) _ = pairColimit :: Colimit (PairF Omega a b) (Coproduct a b)
