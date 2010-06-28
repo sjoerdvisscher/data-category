9cde08997327e423f11001324997d0ac1996de19
Sjoerd Visscher <sjoerd@w3future.com>
2010-06-28 13:04:35 +0200

    Some progress in making the limit adjunction.

--- Data/Category/Adjunction.hs
+++ Data/Category/Adjunction.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE TypeOperators, TypeFamilies, GADTs, FlexibleContexts #-}
+{-# LANGUAGE TypeOperators, TypeFamilies, GADTs, FlexibleContexts, ScopedTypeVariables #-}
 -----------------------------------------------------------------------------
 -- |
 -- Module      :  Data.Category.Adjunction
@@ -17,11 +17,18 @@ import Control.Monad.Instances()
 import Data.Category
 import Data.Category.Functor
 import Data.Category.NaturalTransformation
-
+import Data.Category.Limit
 
 data Adjunction c d f g where
   Adjunction :: (Functor f, Functor g, Category c, Category d, Dom f ~ d, Cod f ~ c, Dom g ~ c, Cod g ~ d) =>
     f -> g -> Nat d d (Id d) (g :.: f) -> Nat c c (f :.: g) (Id c) -> Adjunction c d f g
+
+mkAdjunction :: (Functor f, Functor g, Category c, Category d, Dom f ~ d, Cod f ~ c, Dom g ~ c, Cod g ~ d)
+  => f -> g 
+  -> (forall a. Obj d a -> Component (Id d) (g :.: f) a) 
+  -> (forall a. Obj c a -> Component (f :.: g) (Id c) a)
+  -> Adjunction c d f g
+mkAdjunction f g un coun = Adjunction f g (Nat Id (g :.: f) un) (Nat (f :.: g) Id coun)
 
 unit :: Adjunction c d f g -> Id d :~> (g :.: f)
 unit (Adjunction _ _ u _) = u
@@ -53,7 +60,7 @@ instance Category AdjArrow where
   src (AdjArrow _) = AdjCategory
   tgt (AdjArrow _) = AdjCategory
   
-  id AdjCategory = AdjArrow $ Adjunction Id Id (Nat Id (Id :.: Id) id) (Nat (Id :.: Id) Id id)
+  id AdjCategory = AdjArrow $ mkAdjunction Id Id id id
   
   AdjArrow (Adjunction f g u c) . AdjArrow (Adjunction f' g' u' c') = AdjArrow $ 
     Adjunction (f' :.: f) (g :.: g') (wrap g f u' . u) (c' . cowrap f' g' c)
@@ -69,17 +76,29 @@ cowrap f' g' (Nat (f :.: g) Id n) = Nat ((f' :.: f) :.: (g :.: g')) (f' :.: g') 
 
 
 curryAdj :: Adjunction (->) (->) (EndoHask ((,) e)) (EndoHask ((->) e))
-curryAdj = Adjunction EndoHask EndoHask
-  (Nat Id (EndoHask :.: EndoHask) $ \HaskO -> \a e -> (e, a)) -- unit
-  (Nat (EndoHask :.: EndoHask) Id $ \HaskO -> \(e, f) -> f e) -- counit
+curryAdj = mkAdjunction EndoHask EndoHask (\HaskO -> \a e -> (e, a)) (\HaskO -> \(e, f) -> f e)
 
 
--- limitAdj :: HasLimits j (~>) => Adjunction (Nat j (~>)) (~>) (Diag j (~>)) (LimitFunctor j (~>))
--- limitAdj = Adjunction Diag LimitFunctor 
---   (Nat Id (LimitFunctor :.: Diag) g)
---   (Nat (Diag :.: LimitFunctor) Id f)
---     where
---       f :: HasLimits j (~>) => Obj (Nat j (~>)) a -> Nat j (~>) (Const j (~>) (Limit j a)) a
---       f (NatO a) = coneSurface (limit (NatO a))
---       g :: HasLimits j (~>) => Obj (~>) a -> a ~> Limit j (Const j (~>) a)
---       g a = limitFactorizer (NatO $ Const a) (Cone a (Diag % id a))
+data RightAdjoint :: * -> * -> * where
+  RightAdjoint :: (Functor f, Dom f ~ d, Cod f ~ c, Category c, Category d) => 
+    f -> (forall x. Obj c x -> TerminalUniversal x f (F g x)) -> RightAdjoint f g
+  
+type instance Dom (RightAdjoint f g) = Cod f
+type instance Cod (RightAdjoint f g) = Dom f
+type instance F (RightAdjoint f g) a = F g a
+
+instance Functor (RightAdjoint f g) where
+  RightAdjoint f univ %% x = tuObject (univ x)
+  -- RightAdjoint f univ % h  = terminalFactorizer (univ $ src h) o m
+  --   where
+  --     targetUniv = univ $ tgt h
+  --     o = tuObject targetUniv
+  --     m = terminalMorphism targetUniv . h
+
+terminalPropAdjunction :: RightAdjoint f g -> Adjunction (Cod f) (Dom f) f (RightAdjoint f g) 
+terminalPropAdjunction g@(RightAdjoint f univ) = mkAdjunction f g 
+  (\a -> let fa = f %% a in terminalFactorizer (univ fa) a (id fa))
+  (terminalMorphism . univ)
+
+-- limitAdj :: HasLimits j (~>) => Adjunction (Nat j (~>)) (~>) (Diag j (~>)) (RightAdjoint (Diag j (~>)) (LimitFunctor j (~>)))
+-- limitAdj = terminalPropAdjunction (RightAdjoint Diag limitUniversal :: RightAdjoint (Diag j (~>)) (LimitFunctor j (~>)))
