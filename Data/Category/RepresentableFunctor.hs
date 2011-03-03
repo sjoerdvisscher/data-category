39a0c02846a8c52381afc918c0d4267c6f6462b7
Sjoerd Visscher <sjoerd@w3future.com>
2011-03-03 13:19:08 +0100

    "Representation" has a different use in CT, renamed to Representable.

--- Data/Category/RepresentableFunctor.hs
+++ Data/Category/RepresentableFunctor.hs
@@ -17,33 +17,33 @@ import Data.Category
 import Data.Category.Functor
 
 
-data Representation f repObj = Representation
+data Representable f repObj = Representable
   { representedFunctor :: f
   , representingObject :: Obj (Dom f) repObj
   , represent          :: (Dom f ~ (~>), Cod f ~ (->)) => Obj (~>) z -> f :% z -> repObj ~> z
   , universalElement   :: (Dom f ~ (~>), Cod f ~ (->)) => f :% repObj
   }
 
-unrepresent :: (Functor f, Dom f ~ (~>), Cod f ~ (->)) => Representation f repObj -> repObj ~> z -> f :% z
+unrepresent :: (Functor f, Dom f ~ (~>), Cod f ~ (->)) => Representable f repObj -> repObj ~> z -> f :% z
 unrepresent rep h = representedFunctor rep % h $ universalElement rep
 
-covariantHomRepr :: Category (~>) => Obj (~>) x -> Representation (x :*-: (~>)) x
-covariantHomRepr x = Representation
+covariantHomRepr :: Category (~>) => Obj (~>) x -> Representable (x :*-: (~>)) x
+covariantHomRepr x = Representable
   { representedFunctor = homX_ x
   , representingObject = x
   , represent          = \_ -> id
   , universalElement   = x
   }
 
-contravariantHomRepr :: Category (~>) => Obj (~>) x -> Representation ((~>) :-*: x) x
-contravariantHomRepr x = Representation
+contravariantHomRepr :: Category (~>) => Obj (~>) x -> Representable ((~>) :-*: x) x
+contravariantHomRepr x = Representable
   { representedFunctor = hom_X x
   , representingObject = Op x
   , represent          = \_ h -> Op h
   , universalElement   = x
   }
 
-type InitialUniversal x u a = Representation ((x :*-: Cod u) :.: u) a
+type InitialUniversal x u a = Representable ((x :*-: Cod u) :.: u) a
 -- | An initial universal property, a universal morphism from x to u.
 initialUniversal :: Functor u
                  => u 
@@ -51,14 +51,14 @@ initialUniversal :: Functor u
                  -> Cod u x (u :% a) 
                  -> (forall y. Obj (Dom u) y -> Cod u x (u :% y) -> Dom u a y) 
                  -> InitialUniversal x u a
-initialUniversal u obj mor factorizer = Representation
+initialUniversal u obj mor factorizer = Representable
   { representedFunctor = homX_ (src mor) :.: u
   , representingObject = obj
   , represent          = factorizer
   , universalElement   = mor
   }
   
-type TerminalUniversal x u a = Representation ((Cod u :-*: x) :.: Opposite u) a
+type TerminalUniversal x u a = Representable ((Cod u :-*: x) :.: Opposite u) a
 -- | A terminal universal property, a universal morphism from u to x.
 terminalUniversal :: Functor u
                   => u 
@@ -66,7 +66,7 @@ terminalUniversal :: Functor u
                   -> Cod u (u :% a) x
                   -> (forall y. Obj (Dom u) y -> Cod u (u :% y) x -> Dom u y a) 
                   -> TerminalUniversal x u a
-terminalUniversal u obj mor factorizer = Representation
+terminalUniversal u obj mor factorizer = Representable
   { representedFunctor = hom_X (tgt mor) :.: Opposite u
   , representingObject = Op obj
   , represent          = \(Op y) f -> Op (factorizer y f)
