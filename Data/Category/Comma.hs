ce073e9c409b792a68ac410ede39efde9cfd4f98
Sjoerd Visscher <sjoerd@w3future.com>
2012-07-09 11:31:52 +0200

    Universal properties as initial/terminal objects in comma categories.

--- Data/Category/Comma.hs
+++ Data/Category/Comma.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE TypeOperators, TypeFamilies, MultiParamTypeClasses, GADTs, FlexibleContexts, FlexibleInstances, NoImplicitPrelude #-}
+{-# LANGUAGE TypeOperators, TypeFamilies, MultiParamTypeClasses, GADTs, FlexibleContexts, FlexibleInstances, ScopedTypeVariables, NoImplicitPrelude #-}
 -----------------------------------------------------------------------------
 -- |
 -- Module      :  Data.Category.Comma
@@ -14,6 +14,8 @@ module Data.Category.Comma where
 
 import Data.Category
 import Data.Category.Functor
+import Data.Category.Limit
+import Data.Category.RepresentableFunctor
 
 
 data CommaO :: * -> * -> * -> * where
@@ -28,11 +30,14 @@ data (:/\:) :: * -> * -> * -> * -> * where
     CommaO t s (a', b') ->
     (t :/\: s) (a, b) (a', b')
 
+commaId :: CommaO t s (a, b) -> Obj (t :/\: s) (a, b)
+commaId o@(CommaO a _ b) = CommaA o a b o
+
 -- | The comma category T \\downarrow S
 instance (Category (Dom t), Category (Dom s)) => Category (t :/\: s) where
     
-  src (CommaA so@(CommaO a _ b) _ _ _)    = CommaA so a        b        so
-  tgt (CommaA _ _ _ to@(CommaO a _ b))    = CommaA to a        b        to
+  src (CommaA so _ _ _) = commaId so
+  tgt (CommaA _ _ _ to) = commaId to
   
   (CommaA _ g h to) . (CommaA so g' h' _) = CommaA so (g . g') (h . h') to
 
@@ -42,3 +47,30 @@ type (f `ObjectsFOver`  a) = f :/\: ConstF f a
 
 type (c `ObjectsUnder` a) = Id c `ObjectsFUnder` a
 type (c `ObjectsOver`  a) = Id c `ObjectsFOver`  a
+
+
+initialUniversalComma :: forall u x c a a_
+                       . (Functor u, c ~ (u `ObjectsFUnder` x), HasInitialObject c, (a_, a) ~ InitialObject c)
+                      => u -> InitialUniversal x u a
+initialUniversalComma u = case initialObject :: Obj c (a_, a) of
+  CommaA (CommaO _ mor a) _ _ _ -> 
+    initialUniversal u a mor factorizer
+      where
+        factorizer :: forall y. Obj (Dom u) y -> Cod u x (u :% y) -> Dom u a y
+        factorizer y arr = case (init (commaId (CommaO y arr y))) of CommaA _ _ f _ -> f
+          where
+            init :: Obj c (y, y) -> c (a_, a) (y, y)
+            init = initialize
+
+terminalUniversalComma :: forall u x c a a_
+                        . (Functor u, c ~ (u `ObjectsFOver` x), HasTerminalObject c, (a, a_) ~ TerminalObject c)
+                       => u -> TerminalUniversal x u a
+terminalUniversalComma u = case terminalObject :: Obj c (a, a_) of
+  CommaA (CommaO a mor _) _ _ _ ->
+    terminalUniversal u a mor factorizer
+      where
+        factorizer :: forall y. Obj (Dom u) y -> Cod u (u :% y) x -> Dom u y a
+        factorizer y arr = case (term (commaId (CommaO y arr y))) of CommaA _ f _ _ -> f
+          where
+            term :: Obj c (y, y) -> c (y, y) (a, a_)
+            term = terminate
